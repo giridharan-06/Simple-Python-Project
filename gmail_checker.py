0437048a36594051feda9d3085da7549438e61f7
Giridharan S <giridharansasikumar06@gmail.com>
2026-07-08 15:33:51 +0530

    Add Gmail address validation function
    Implement basic validation for Gmail addresses.

--- gmail_checker.py
+++ gmail_checker.py
@@ -1,2 +1,11 @@
 def mail_address(address):
-    pass
+    dot=address.find('.')
+    at=address.find('@')
+    if dot==-1:
+        print("Invalid Gmail")
+    else:
+        print("Valid")
+
+while(True):
+    print("Enter the Corret Gmail Address")
+    x=input("Enter the gmail address: ")
